d39d84fb74332c907c2a00f1ceaced83fa9d62b1
Janmarc Peconcillo <janmarc.peconcillo@gmail.com>
2016-08-17 17:44:34 -0700

    Initial Commit

---
+++ GIF Search App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GIF Search App
+//
+//  Created by Janmarc on 8/17/16.
+//  Copyright © 2016 Nero. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
